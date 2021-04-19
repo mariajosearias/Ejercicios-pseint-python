438b3502f35f1100a42b995591842fb73a6b83cb
María José José <>
2021-04-19 20:47:35 +0000

    Ejercicio 1

---
+++ Taller 2/Ejercicio1.py
@@ -0,0 +1,15 @@
+"""
+Entradas
+edad1-->float-->e1
+edad2-->float-->e2
+edad3-->float-->e3
+Salidas
+promedio-->float-->p
+"""
+e1=int(input("Ingrese la edad #1"))
+e2=int(input("Ingrese la edad #2"))
+e3=int(input("Ingrese la edad #3"))
+
+p=((e1+e2+e3)/3)
+
+print ("El promedio de las edades es: " +str(p))
